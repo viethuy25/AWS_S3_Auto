da1da27135f19392d31b196dcbe3a1fc88d71a41
James Nguyen <viethuy25@gmail.com>
2020-09-13 14:00:55 -0400

    auto create bucket and add file

--- aws_utils.py
+++ aws_utils.py
@@ -5,6 +5,7 @@ Created on Sat Sep 12 13:13:12 2020
 @author: vieth
 """
 import boto3
+import pandas as pd
 import csv
 
 def s3_conn(key_id, secret_key, regional_name):
@@ -28,12 +29,28 @@ def upload_dataset(s3, bucket_name, file_name):
     #upload files to S3 bucket
     bucket.upload_file(file_name, Key='Iris.csv')
     
-def read_csv (s3,bucket_name, file_name):
+def read_csv (s3,bucket_name, file_name):    
+    bucket = s3.Bucket(bucket_name) # example: energy_market_procesing
     
-obj = s3.get_object(Bucket= bucket, Key= file_name) 
-# get object and file (key) from bucket
-
-initial_df = pd.read_csv(obj['Body']) # 'Body' is a key wor
+    initial_df = pd.DataFrame()
+    # Iterates through all the objects, doing the pagination for you. Each obj
+    # is an ObjectSummary, so it doesn't contain the body. You'll need to call
+    # get to get the whole body.
+    for obj in bucket.objects.all():
+        key = obj.key
+        body = obj.get()['Body'].read()
+        print (key)
+        print (file_name)
+        
+        if key.lower()==file_name.lower():
+            body = obj.get()['Body']#.read()
+            initial_df = pd.read_csv(body) #obj['Body']) # 'Body' is a key wor
+    
+    #obj = s3.get_object(Bucket= bucket_name, Key= file_name) 
+    
+    #initial_df = pd.read_csv(obj['Body']) # 'Body' is a key wor
+    
+    return initial_df
 
 def new_all(s3, bucket_name, file_name, region_name):
     location = {'LocationConstraint': region_name}
--- main.py
+++ main.py
@@ -6,17 +6,23 @@ Created on Sat Sep 12 12:24:38 2020
 """
 
 import aws_utils
+import json
 
-key_id = 'AKIAR7OANEE3C4GP2AHP'
-secret_key = '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'
-region_name = 'us-east-2'
+with open('data/bucket_policy.json',) as f:
+    data = json.load(f)
+
+# Output: {'key_id': , "secret_key" : , "region_name" : }
+print(data)
 
 file_name = 'iris.csv'
-s3 = aws_utils.s3_conn(key_id, secret_key, region_name)
+s3 = aws_utils.s3_conn(data['key_id'], data['secret_key'], data['region_name'])
 
 bucket_name = 'newbie-test-1'
 
 #aws_utils.upload_dataset(s3, bucket_name, file_name)
 #aws_utils.new_all(s3, 'new-test-1', file_name, region_name)
 
+file = aws_utils.read_csv(s3, bucket_name, file_name)
+
 print (s3)
+print (file)
